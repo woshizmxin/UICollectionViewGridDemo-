e810392afb23977f84eb1dd398aacb0dbddb4edd
周茂 <zhoumao@meituan.com>
2015-11-15 10:42:44 +0800

    first commit

---
+++ UICollectionViewDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  UICollectionViewDemo
+//
+//  Created by zhoumao on 15/11/15.
+//  Copyright © 2015年 zhoumao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+    
+}
+@property (strong, nonatomic)UICollectionView *collectionView;
+
+@end
